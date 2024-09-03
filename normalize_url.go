cc542d47588530b8e460792b2fb49832ae9dfdfe
bpoche <brettdp@gmail.com>
2024-09-03 10:27:58 -0500

    chore: moved files

---
+++ normalize_url.go
@@ -0,0 +1,18 @@
+package normalize_url
+
+import (
+	"net/url"
+	"strings"
+)
+
+func normalizeURL(rawURL string) (string, error) {
+	parsedURL, err := url.Parse(rawURL)
+	if err != nil {
+		return "", err
+	}
+	// extract host and path and make lowercase
+	fullPath := parsedURL.Host + parsedURL.Path
+	fullPath = strings.ToLower(fullPath)
+	fullPath = strings.TrimSuffix(fullPath, "/")
+	return fullPath, nil
+}
